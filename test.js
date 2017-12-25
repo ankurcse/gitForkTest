f8f7143bef0677aa469e8fc4ebf564280ceb3ee5
ankurcse <ankurcse1989@gmail.com>
2017-12-25 11:27:18 +0530

    Added new Changes #f1.0

---
+++ test.js
@@ -0,0 +1,22 @@
+function User(data){
+	
+	this.name = data.name;
+	this.age = data.age;
+	
+	this.getUserDetail = function(){
+		
+		return "Name:-"+this.name +"   "+"Age:"+this.age;
+	}
+	
+}
+User ankur = new User({name:"Ankur",age:27});
+console.log(ankur.getUserDetail());
+
+User.prototype.getUserData = function(){
+	
+	return  "Name:-"+this.name +"   "+"Age:"+this.age ;
+}
+
+function commonUser(){
+	
+}
